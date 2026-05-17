54f9db4967f3f2946b1ad4e97a416efe864cd82a
Felipe <Fel.fariass@duocuc.cl>
2026-05-16 23:21:31 -0400

    conexion veterinario}

---
+++ src/app/service/veterinario-service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { Veterinario } from '../models/veterinario.model';
+@Injectable({
+  providedIn: 'root',
+})
+export class VeterinarioService {
+
+  private http = inject(HttpClient);
+
+  obtenerVeterinarios(): Observable<Veterinario[]> {
+    return this.http.get<Veterinario[]>(environment.apiUrlVeterinarios);
+  }
+
+  crearVeterinario(veterinario: Veterinario): Observable<Veterinario> {
+    return this.http.post<Veterinario>(environment.apiUrlVeterinarios, veterinario);
+  }
+
+  editarVeterinario(veterinario: Veterinario): Observable<Veterinario> {
+    return this.http.put<Veterinario>(`${environment.apiUrlVeterinarios}/${veterinario.id}`, veterinario);
+  }
+
+  eliminarVeterinario(id: number): Observable<void> {
+    return this.http.delete<void>(`${environment.apiUrlVeterinarios}/${id}`);
+  }
+}
